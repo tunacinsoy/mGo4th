69d9448a7fea5d768ea788cb8cb173f9e005dc8c
tuna <tunacinsoy@gmail.com>
2024-09-30 15:27:46 +0200

    feat: crypto rand package

--- ch02/24-cryptoRand/cryptoRand.go
+++ ch02/24-cryptoRand/cryptoRand.go
@@ -1,3 +1,4 @@
+// The aim of this program is to get familiar with crypto/rand package
 package main
 
 import (
@@ -25,24 +26,28 @@ func generatePass(s int64) (string, error) {
 	}
 	return base64.URLEncoding.EncodeToString(b), nil
 }
-
 func main() {
 	var LENGTH int64 = 8
-	arguments := os.Args
+
+	arguments := os.Args[1:]
+
 	switch len(arguments) {
-	case 2:
-		t, err := strconv.ParseInt(os.Args[1], 10, 64)
-		if err == nil {
+	case 1:
+		t, err := strconv.ParseInt(arguments[0], 10, 64)
+
+		if err != nil {
+			fmt.Printf("An error occurred during the conversion to int of os arg: %v\n", arguments[0])
+		}
+
+		if LENGTH > 0 {
 			LENGTH = t
 		}
-		if LENGTH <= 0 {
-			LENGTH = 8
-		}
 	default:
-		fmt.Println("Using default values!")
+		fmt.Println("Using default value for length, which is 8")
 	}
 
 	myPass, err := generatePass(LENGTH)
+
 	if err != nil {
 		fmt.Println(err)
 		return

272fb1724429f4f1b39bdb491ce8a8d0579b7cc4
tuna <tunacinsoy@gmail.com>
2024-10-07 12:46:00 +0200

    feat: custom interface for generic functions

--- ch04/03-numeric/numeric.go
+++ ch04/03-numeric/numeric.go
@@ -1,21 +1,20 @@
+// The aim of this program is to learn how to apply custom interface for generic function constraints
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type Numeric interface {
 	int | int8 | int16 | int32 | int64 | float64
 }
 
-func Add[T Numeric](a, b T) T {
+func Add[T Numeric](a T, b T) T {
 	return a + b
 }
 
 func main() {
+
 	fmt.Println("4 + 3 =", Add(4, 3))
 	fmt.Println("4.1 + 3.2 =", Add(4.1, 3.2))
+	fmt.Println("4.1 + 3 =", Add(4.1, 3))
 
-	// This is not going to work
-	// _ = Add(4.1, 3)
 }

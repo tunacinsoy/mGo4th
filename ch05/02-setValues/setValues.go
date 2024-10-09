bec7103f4d1ade43d588c22b6c7c28c63f3c00ee
tuna <tunacinsoy@gmail.com>
2024-10-09 22:04:19 +0200

    feat: set values with reflect package

--- ch05/02-setValues/setValues.go
+++ ch05/02-setValues/setValues.go
@@ -1,3 +1,5 @@
+// The aim of this program is to dynamically changing the values of the fields of a structure without knowing the internals of the structure in advance.
+// "Dynamically" in this context means that the code can access or modify the structure's fields at runtime, rather than being explicitly defined at compile-time
 package main
 
 import (
@@ -15,6 +17,7 @@ func main() {
 	A := T{1, "F2", 3.0}
 	fmt.Println("A:", A)
 
+	// With the use of Elem() and a pointer to variable A, variable A can be modified if needed.
 	r := reflect.ValueOf(&A).Elem()
 	fmt.Println("String value:", r.String())
 	typeOfA := r.Type()
@@ -30,6 +33,4 @@ func main() {
 			r.Field(i).SetString("Changed!")
 		}
 	}
-
-	fmt.Println("A:", A)
 }

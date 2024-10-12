5d19e7a1abef98a5bb820ebc296d56b49278e88d
tuna <tunacinsoy@gmail.com>
2024-10-12 12:58:53 +0200

    feat: error interface

--- ch05/09-errorInt/errorInt.go
+++ ch05/09-errorInt/errorInt.go
@@ -1,8 +1,8 @@
+// The aim of this program is to understand custom error definitions by implementing error interface
 package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -13,20 +13,19 @@ type emptyFile struct {
 	Read  int
 }
 
-// Implement error interface
 func (e emptyFile) Error() string {
-	return fmt.Sprintf("Ended with io.EOF (%t) but read (%d) bytes", e.Ended, e.Read)
+	return fmt.Sprintf("Ended with io.EOF (%v) but read (%d) bytes", e.Ended, e.Read)
 }
 
-// Check values
 func isFileEmpty(e error) bool {
-	// Type assertion
 	v, ok := e.(emptyFile)
+
 	if ok {
 		if v.Read == 0 && v.Ended == true {
 			return true
 		}
 	}
+
 	return false
 }
 
@@ -42,6 +41,7 @@ func readFile(file string) error {
 	n := 0
 	for {
 		line, err := reader.ReadString('\n')
+		//fmt.Printf("line: %v", line)
 		n += len(line)
 		if err == io.EOF {
 			// End of File: nothing more to read
@@ -57,13 +57,14 @@ func readFile(file string) error {
 }
 
 func main() {
-	flag.Parse()
-	if len(flag.Args()) == 0 {
-		fmt.Println("usage: errorInt <file1> [<file2> ...]")
-		return
+	// cmd to run: go run errorInt.go /etc/hosts /tmp/doesnotexist /tmp/empty /tmp /tmp/Empty.txt emptyTxt.txt notEmptyTxt.txt
+
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println("usage errorInt <file1>  [<file2> ...]")
 	}
 
-	for _, file := range flag.Args() {
+	for _, file := range args {
 		err := readFile(file)
 		if isFileEmpty(err) {
 			fmt.Println(file, err)
@@ -73,4 +74,14 @@ func main() {
 			fmt.Println(file, "is OK.")
 		}
 	}
+
+	// Outputs
+	// /etc/hosts is OK.
+	// /tmp/doesnotexist open /tmp/doesnotexist: no such file or directory
+	// /tmp/empty open /tmp/empty: no such file or directory
+	// /tmp read /tmp: is a directory
+	// /tmp/Empty.txt open /tmp/Empty.txt: no such file or directory
+	// emptyTxt.txt Ended with io.EOF (true) but read (0) bytes
+	// notEmptyTxt.txt is OK.
+
 }

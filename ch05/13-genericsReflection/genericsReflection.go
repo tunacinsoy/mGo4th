b7d61bcd2920c5c3eeb82130f71b5459b249c165
tuna <tunacinsoy@gmail.com>
2024-10-12 16:32:04 +0200

    feat: generics vs reflect package

--- ch05/13-genericsReflection/genericsReflection.go
+++ ch05/13-genericsReflection/genericsReflection.go
@@ -1,3 +1,5 @@
+// The aim of this program is to compare generics with reflect package implementations
+// Reflect makes the code harder to understand, use generics as much as possible
 package main
 
 import (
@@ -12,17 +14,20 @@ func PrintReflection(s interface{}) {
 	if val.Kind() != reflect.Slice {
 		return
 	}
-
 	for i := 0; i < val.Len(); i++ {
 		fmt.Print(val.Index(i).Interface(), " ")
 	}
 	fmt.Println()
 }
 
+// Once again, the implementation of the generic function is simpler and therefore easier to understand.
+// Moreover, the function signature specifies that only slices are accepted as function parameters.
+// We do not have to perform any additional checks for that as this is a job for the Go compiler.
 func PrintSlice[T any](s []T) {
 	fmt.Println("** Generics")
+
 	for _, v := range s {
-			fmt.Print(v, " ")
+		fmt.Print(v, " ")
 	}
 	fmt.Println()
 }
@@ -35,4 +40,5 @@ func main() {
 	PrintReflection([]int{1, 2, 3})
 	PrintReflection([]string{"a", "b", "c"})
 	PrintReflection([]float64{1.2, -2.33, 4.55})
+
 }

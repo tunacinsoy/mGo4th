ff3bc69e04c2340c6f543632bb6fcbe7a38c500f
tuna <tunacinsoy@gmail.com>
2024-11-08 16:42:06 +0100

    feat: bespoke addition

--- ch07/08-encodeDecode/encodeDecode.go
+++ ch07/08-encodeDecode/encodeDecode.go
@@ -21,6 +21,7 @@ type UseAll struct {
 func main() {
 	useall := UseAll{Name: "Mike", Surname: "Wazowski", Year: 2023}
 
+	// Let's do marshaling (converting the UseAll struct into JSON Format)
 	t, err := json.Marshal(&useall)
 	if err != nil {
 		fmt.Println(err)
@@ -28,16 +29,20 @@ func main() {
 		fmt.Printf("Value %s\n", t) // Value {"username":"Mike","surname":"Wazowski","created":2023}
 	}
 
-	str := `{"username": "T.", "surname": "Cns", "created": 2024}`
-	jsonRecord := []byte(str)
+	// Let's unmarshal what we marshaled before (converting the JSON Format into UseAll struct)
 
 	temp := UseAll{}
 
-	err = json.Unmarshal(jsonRecord, &temp)
+	// str := `{"username": "T.", "surname": "Cns", "created": 2024}`
+	// jsonRecord := []byte(str)
+	// err = json.Unmarshal(jsonRecord, &temp)
+
+	err = json.Unmarshal(t, &temp)
+
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Printf("Data type: %T, with value %v\n", temp, temp) // Data type: main.UseAll, with value {T. Cns 2024}
+		fmt.Printf("Data type: %T, with value %v\n", temp, temp) // Data type: main.UseAll, with value {Mike Wazowski 2023}
 	}
 
 }
